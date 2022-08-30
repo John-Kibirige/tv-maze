4da4c486066762e0f8b5c1e7bda3e772846e4832
melashu <meshu102@outlook.com>
2022-08-30 22:29:09 +0300

    Add event listner

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './styles/tvmaze.css';
-// import logo from '../images/logo.png';
+import * as dialog from './module/comment';
+
+
+const addComment = document.querySelector(".add-comment");
+document.querySelector(".all-comment").addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+dialog.showPopupDialog(0)
+});
+
+addComment.addEventListener('click', () => {
+    alert('add comment');
+})
+
