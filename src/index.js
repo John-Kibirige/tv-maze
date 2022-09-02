db5d40ace4913861dee2208dd3a5b8b571c5e9e7
melashu <meshu102@outlook.com>
2022-09-02 12:42:01 +0300

    update index js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import './styles/tvmaze.css';
 import * as dialog from './module/comment';
 
 
-const addComment = document.querySelector(".add-comment");
 document.querySelector(".all-comment").addEventListener('click', (e) => {
     const id = e.target.dataset.id;
-dialog.showPopupDialog(0)
+    dialog.showPopupDialog(id)
 });
 
-addComment.addEventListener('click', () => {
-    alert('add comment');
-})
+
 
