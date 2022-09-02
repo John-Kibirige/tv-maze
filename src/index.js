32aa48405ffc584c4a3adf744d184a802efbf63e
melashu <meshu102@outlook.com>
2022-09-02 13:23:45 +0300

    fix js  linter error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import './styles/tvmaze.css';
-import * as dialog from './module/comment';
+import showPopupDialog from './module/comment.js';
 
-
-document.querySelector(".all-comment").addEventListener('click', (e) => {
-    const id = e.target.dataset.id;
-    dialog.showPopupDialog(id)
+document.querySelector('.all-comment').addEventListener('click', (e) => {
+  const { id } = e.target.dataset;
+  showPopupDialog(id);
 });
-
-
-
