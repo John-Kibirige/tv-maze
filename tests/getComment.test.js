69776364dfdb030166b0f7658e7609f1628fab68
melashu <meshu102@outlook.com>
2022-09-02 09:04:02 +0300

    create test for get comment

---
+++ tests/getComment.test.js
@@ -0,0 +1,28 @@
+import { getComment } from "../src/module/getComment";
+
+const commentList = [
+  { item_id: 1, username: "meshu", comment: "Love it seriously" },
+];
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(commentList),
+  })
+);
+
+describe("Test Get comment function", () => {
+  it("Retrive comment", async () => {
+    const commentList = await getComment(1);
+    expect(commentList.length).toBe(1);
+  });
+
+  it("Number of Fetch call", async () => {
+    const commentList = await getComment(2);
+    expect(fetch).not.toBeCalledTimes(3);
+  });
+
+  it("Number of Fetch call", async () => {
+    const commentList = await getComment(1);
+    expect(fetch).toBeCalledTimes(3);
+  });
+});
