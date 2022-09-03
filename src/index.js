841c21f9e63bdf53fc8fa12357677ee3c5f97f1c
melashu <meshu102@outlook.com>
2022-09-03 10:22:03 +0300

    call show showPopupDialog

--- src/index.js
+++ src/index.js
@@ -57,7 +57,8 @@ const displayMovies = async () => {
     // implement the comment feature
     document.querySelector('.show-items').addEventListener('click', (e) => {
       if (e.target.classList.contains('comment-btn')) {
-        // code
+        const id = e.target.dataset;
+         showPopupDialog(id['id']);
       }
     });
 
@@ -71,34 +72,30 @@ window.addEventListener('load', () => {
   displayMovies();
 });
 
-document.querySelector('.all-comment').addEventListener('click', (e) => {
-  const { id } = e.target.dataset;
-  showPopupDialog(id);
-});
 
 popup.addEventListener('click', (e) => {
   e.preventDefault();
-  if (e.target.classList.contains('add-comment')) {
-    const userName = document.querySelector('input').value;
-    const message = document.querySelector('textarea').value;
-    const emptyUser = document.querySelector('.empty-name');
-    const emptyComment = document.querySelector('.empty-comment');
-
-    if (userName === '' && message === '') {
+  if (e.target.classList.contains("add-comment")) {
+    const userName = document.querySelector("input").value;
+    const message = document.querySelector("textarea").value;
+    const emptyUser = document.querySelector(".empty-name");
+    const emptyComment = document.querySelector(".empty-comment");
+    const id = e.target.dataset.id;
+    if (userName === "" && message === "") {
       Display.blockComment(emptyComment);
       Display.blockUser(emptyUser);
-    } else if (userName === '') {
+    } else if (userName === "") {
       Display.blockUser(emptyUser);
       Display.noneComment(emptyComment);
-    } else if (message === '') {
+    } else if (message === "") {
       Display.blockComment(emptyComment);
       Display.noneUser(emptyUser);
     } else {
-      addComment(e.target.dataset.id, userName, message);
+      addComment(id, userName, message);
       Display.noneUser(emptyUser);
       Display.noneComment(emptyComment);
-      document.querySelector('input').value = '';
-      document.querySelector('textarea').value = '';
+      document.querySelector("input").value = "";
+      document.querySelector("textarea").value = "";
     }
   }
 });
